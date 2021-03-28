2c9a029dab41b050e88a6c534a9c2e3d0b45b30c
Unknown <felipe.fugimoto3@gmail.com>
2021-03-28 20:42:39 -0300

    Transferidos

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+
+public class Main {
+    public static class  Janela  extends JFrame{
+        public Janela(){
+
+            setTitle("Compilador");
+            setSize(500,400);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLocationRelativeTo(null);
+            setVisible(true);
+
+        }
+
+    }
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+    }
+}
