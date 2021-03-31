747eb87ee9e82d698d2c95a87aca778f2a8855b3
Unknown <felipe.fugimoto3@gmail.com>
2021-03-30 21:56:30 -0300

    Foi construído o painel e os botões de apagar, voltar e gerar. Tambem foi contruido dois paineis de texto um para gerar codigo e outro para imprimir

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,53 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Main {
-    public class  Janela  extends JFrame{
-        public Janela(){
 
-            setTitle("Compilador");
-            setSize(500,400);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setLocationRelativeTo(null);
-            setVisible(true);
-
-        }
-
-    }
     public static void main(String[] args) {
-        Janela janela = new Janela();
+
+            JFrame janela= new JFrame("Compilador");
+            JPanel painelCodigo = new JPanel();
+            JTextArea texto= new JTextArea();
+            JTextArea Paineltexto= new JTextArea();
+
+            painelCodigo.setLayout(null);
+
+            JButton gerar= new JButton("Depurar");
+            JButton retornar = new JButton("Voltar");
+            JButton apagar= new JButton("Apagar");
+
+
+
+            //Painel
+
+
+            painelCodigo.setBackground( Color.BLACK );
+            janela.add(painelCodigo);
+
+            //Botoes
+            painelCodigo.add(gerar);
+            painelCodigo.add(retornar);
+            painelCodigo.add(apagar);
+            gerar.setBounds(875,20,80,60);
+            retornar.setBounds(875,100,80,60);
+            apagar.setBounds(875,180,80,60);
+
+
+            //Texto
+            painelCodigo.add(texto);
+            painelCodigo.add(Paineltexto);
+            texto.setBounds(10,10,850,600);
+            Paineltexto.setBounds(10,680,950,300);
+
+            //Tela principal
+            janela.setBounds(150,35,1000,1050);
+            janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            janela.setLocationRelativeTo(null);
+
+            janela.setVisible(true);
+
+
+
     }
 }
